14e6611fbe7d83abf598e583422a3197deb0ee7c
My Name <viverlin7@gmail.com>
2024-04-09 19:25:07 -0600

    limit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,9 +4,9 @@ const productSchema = require("../models/products");
 const products = require("../models/products");
 
 const getProducts = async (req, res) => {
-    const { featured, company, name, sort: sortBy, fields } = req.query;
+    const { featured, company, name, sort: sortBy, fields, limit } = req.query;
     const queryObject = {};
-console.log(req.query)
+
     if (featured) {
         queryObject.featured = featured === "true" ? true : false;
     }
@@ -37,9 +37,16 @@ console.log(req.query)
         results = results.select(fieldsList)
     }
 
+    // hit limiting logic
+    if (limit) {
+        results = results.limit(Number(limit));
+    }
+
+    // results creates the database query and we await for products for the database response bades on the query
 
     const products = await results
-    return res.status(200).json({ msg: "testing routes", data: products })
+    console.log(products)
+    return res.status(200).json({ NumHits: products.length, data: products })
 }
 
 const getProductsStatic = async (req, res) => {
